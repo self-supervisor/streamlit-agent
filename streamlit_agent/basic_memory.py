a53060d604a9d24063af45ddff5e07964f2b4d51
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-11-25 17:16:45 +0000

    back to square one

--- streamlit_agent/basic_memory.py
+++ streamlit_agent/basic_memory.py
@@ -15,40 +15,9 @@ def load_conversations(file_path):
                 if line.startswith("Nora:"):
                     message = line.split(":", 1)[1].strip().strip('"')
                     msgs.add_ai_message(message)
-                elif line.startswith("Sarah:"):
+                elif line.startswith("Sarah"):
                     message = line.split(":", 1)[1].strip().strip('"')
                     msgs.add_user_message(message)
-    return msgs
-
-
-def load_conversations_some(file_path, max_messages=10):
-    msgs = StreamlitChatMessageHistory(key="langchain_messages")
-    if os.path.exists(file_path):
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-            # Keep only the last 'max_messages' lines
-            recent_lines = lines[-max_messages:]
-
-            for line in recent_lines:
-                if line.startswith("Nora:"):
-                    message = line.split(":", 1)[1].strip().strip('"')
-                    msgs.add_ai_message(message)
-                elif line.startswith("Sarah:"):
-                    message = line.split(":", 1)[1].strip().strip('"')
-                    msgs.add_user_message(message)
-    return msgs
-
-
-def truncate_messages(msgs, max_messages=100):
-    if len(msgs.messages) > max_messages:
-        # Create a new StreamlitChatMessageHistory instance with the last 'max_messages' messages
-        truncated_msgs = StreamlitChatMessageHistory(key="langchain_messages")
-        for msg in msgs.messages[-max_messages:]:
-            if msg.type == "ai":
-                truncated_msgs.add_ai_message(msg.content)
-            else:
-                truncated_msgs.add_user_message(msg.content)
-        return truncated_msgs
     return msgs
 
 
@@ -60,7 +29,7 @@ I'm Nora, your AI Doula. I'm all about giving you the info, support, and a liste
 """
 
 # Set up memory
-msgs = load_conversations_some("streamlit_agent/conversation_history.txt")
+msgs = load_conversations("streamlit_agent/conversation_history.txt")
 memory = ConversationBufferMemory(chat_memory=msgs)
 if len(msgs.messages) == 0:
     msgs.add_ai_message("How have you been?")
@@ -68,10 +37,10 @@ if len(msgs.messages) == 0:
 view_messages = st.expander("View the message contents in session state")
 
 # Get an OpenAI API Key before continuing
-if "openai_api_key" in st.secrets:
-    openai_api_key = st.secrets.openai_api_key
-else:
-    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+# if "openai_api_key" in st.secrets:
+#     openai_api_key = st.secrets.openai_api_key
+# else:
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Enter an OpenAI API Key to continue")
     st.stop()
@@ -93,7 +62,6 @@ for msg in msgs.messages:
 
 # If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
-    # msgs = truncate_messages(msgs, max_messages=10)
     st.chat_message("human").write(prompt)
     # Note: new messages are saved to history automatically by Langchain during run
     response = llm_chain.run(prompt)
