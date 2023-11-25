4dc98ff39c4c909785a45d08a323186d3b15940b
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-11-25 18:46:16 +0000

    trying example

--- streamlit_agent/basic_memory.py
+++ streamlit_agent/basic_memory.py
@@ -76,7 +76,7 @@ memory.save_context(
 #     memory=memory,
 #     verbose=True,
 # )
-llm = OpenAI(temperature=0)  # Can be any valid LLM
+llm = OpenAI(openai_api_key=openai_api_key,temperature=0)  # Can be any valid LLM
 _DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
 Relevant pieces of previous conversation:
