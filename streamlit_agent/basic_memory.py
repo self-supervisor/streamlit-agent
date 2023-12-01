59b9b2779fd1b2c6e929ead8a0bf9a19f6f6a2a2
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-12-01 01:29:18 +0000

    loading profile into memory

--- streamlit_agent/basic_memory.py
+++ streamlit_agent/basic_memory.py
@@ -50,10 +50,8 @@ def load_memory(file_path, memory_object):
 def load_profile_into_memory(file_path, memory_object):
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
-            print("adding to memory input")
             for line in file:
                 if ":" in line:
-                    # Check if there is a previous human input that hasn't been paired yet
                     line_split = line.split(":", 1)
                     input = line_split[0]
                     output = line_split[1]
@@ -86,8 +84,8 @@ vectorstore = FAISS(embedding_fn, index, InMemoryDocstore({}), {})
 # the vector lookup still returns the semantically relevant information
 retriever = vectorstore.as_retriever(search_kwargs=dict(k=1))
 memory = VectorStoreRetrieverMemory(retriever=retriever)
-memory = load_memory("streamlit_agent/elder_conversation.txt", memory)
-# memory = load_profile_into_memory("streamlit_agent/elder_profile.txt", memory)
+# memory = load_memory("streamlit_agent/elder_conversation.txt", memory)
+memory = load_profile_into_memory("streamlit_agent/elder_profile.txt", memory)
 
 llm = OpenAI(openai_api_key=openai_api_key, temperature=0)  # Can be any valid LLM
 _DEFAULT_TEMPLATE = """
